0eba79e4cff769cca18a352a4068b5b4a6b84063
Saif Gsouri <saifeddine.gsouri@ims.co.at>
2026-04-14 14:12:28 +0200

    fix: bump SW cache to v3 with correct assets, remove missing icons

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'life-logger-v1';
-const ASSETS = ['./', './index.html', './css/style.css', './js/db.js', './js/github.js', './js/app.js'];
+const CACHE = 'life-logger-v3';
+const ASSETS = ['./', './index.html', './css/style.css', './js/github.js', './js/whisper.js', './js/app.js'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
